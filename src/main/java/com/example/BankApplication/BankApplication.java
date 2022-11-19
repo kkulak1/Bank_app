4161e6217b865b9cbfd7aadd8875f7d7c5a2b2ed
kkulak1 <01169200@pw.edu.pl>
2022-11-19 15:14:22 +0100

    Project ready to start

---
+++ src/main/java/com/example/BankApplication/BankApplication.java
@@ -0,0 +1,21 @@
+package com.example.BankApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplication.class, args);
+	}
+
+		@GetMapping
+
+		public String hello(){
+			return "Siema";
+		}
+}
--- src/test/java/com/example/BankApplication/BankApplicationTests.java
+++ src/test/java/com/example/BankApplication/BankApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bank;
+package com.example.BankApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
