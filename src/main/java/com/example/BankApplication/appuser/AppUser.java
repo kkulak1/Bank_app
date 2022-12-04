cfce72b1b6de2a6fad7e7e1a308eb00584edd502
kkulak1 <01169200@pw.edu.pl>
2022-12-04 12:38:02 +0100

    implemented registration service

--- src/main/java/com/example/BankApplication/appuser/AppUser.java
+++ src/main/java/com/example/BankApplication/appuser/AppUser.java
@@ -4,7 +4,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import net.bytebuddy.build.Plugin;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -33,23 +32,21 @@ public class AppUser implements UserDetails {
     )
 
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled=false;
 
-    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
-        this.name = name;
-        this.username = username;
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @Override
@@ -65,7 +62,15 @@ public class AppUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 
     @Override
--- src/main/java/com/example/BankApplication/registration/RegistrationService.java
+++ src/main/java/com/example/BankApplication/registration/RegistrationService.java
@@ -1,11 +1,29 @@
 package com.example.BankApplication.registration;
 
+import com.example.BankApplication.appuser.AppUser;
+import com.example.BankApplication.appuser.AppUserRole;
+import com.example.BankApplication.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
 
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid!");
+        }
+
+        return appUserService.signUpUser(new AppUser(
+                request.getFirstName(),
+                request.getLastName(),
+                request.getEmail(),
+                request.getPassword(),
+                AppUserRole.USER
+        ));
     }
 }
