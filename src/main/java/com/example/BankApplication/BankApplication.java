babcfbc6402d5b92114b6661bbb53dbee51f53d3
kkulak1 <01169200@pw.edu.pl>
2022-12-05 13:28:07 +0100

    email added, emails sending doesnt work yet

--- src/main/java/com/example/BankApplication/BankApplication.java
+++ src/main/java/com/example/BankApplication/BankApplication.java
@@ -26,7 +26,29 @@ public class BankApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
 	}
+<<<<<<< HEAD
 	//TODO:napraw wysylanie maili
+=======
+
+//	@Bean
+//	public JavaMailSender getJavaMailSender() {
+//		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+//		mailSender.setHost("smtp.gmail.com");
+//		mailSender.setPort(587);
+//
+//		mailSender.setUsername("projektpapz28@gmail.com");
+//		mailSender.setPassword("trudne@haslo");
+//
+//		Properties props = mailSender.getJavaMailProperties();
+//		props.put("mail.transport.protocol", "smtp");
+//		props.put("mail.smtp.auth", "true");
+//		props.put("mail.smtp.starttls.enable", "true");
+//		props.put("mail.debug", "true");
+//
+//		return mailSender;
+//	}
+//	TODO: repair sending emails
+>>>>>>> 0392dbe (email added, emails sending doesnt work yet)
 }
 
 
