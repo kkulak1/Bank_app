8d17a8f2a6b01660d37a3b4db314049e609abbd8
Hubert Brzóskniewicz <hubert.brzoskniewicz@gmail.com>
2022-12-06 17:54:56 +0100

    index_html

---
+++ src/main/java/com/example/BankApplication/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.BankApplication.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@ComponentScan
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
+    }
+}
